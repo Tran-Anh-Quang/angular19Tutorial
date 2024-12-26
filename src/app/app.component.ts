58ce4bf214ef0ca6212e45822ff0714a77b39327
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-12-26 22:04:59 +0700

    part 2: how to create a component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AdminComponent } from "./components/admin/admin.component";
+import { UserComponent } from './components/user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AdminComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
