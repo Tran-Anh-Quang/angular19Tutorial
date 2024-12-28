22f67869f6578f9d361eb0c6173a1f19fefef111
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-12-28 23:18:58 +0700

    part 4: ngIf example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { DataBindingComponent } from "./components/data-binding/data-binding.component";
+import { AdminComponent } from "./components/admin/admin.component";
+import { NgIfComponent } from "./components/ng-if/ng-if.component";
 
 @Component({
   selector: 'app-root',
-  imports: [DataBindingComponent],
+  imports: [DataBindingComponent, AdminComponent, NgIfComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
