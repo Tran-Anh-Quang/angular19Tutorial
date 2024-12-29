e550c6a1a9463cd33f683d350416a4160d87c589
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-12-29 21:18:27 +0700

    part 5: ngClass sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { DataBindingComponent } from "./components/data-binding/data-binding.component";
-import { AdminComponent } from "./components/admin/admin.component";
-import { NgIfComponent } from "./components/ng-if/ng-if.component";
-import { NgForComponent } from "./components/ng-for/ng-for.component";
+
+import { NgClassComponent } from "./components/ng-class/ng-class.component";
 
 @Component({
   selector: 'app-root',
-  imports: [DataBindingComponent, AdminComponent, NgIfComponent, NgForComponent],
+  imports: [NgClassComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
