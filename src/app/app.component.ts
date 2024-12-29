7d59fa4a8504fe28888afd6848a83263d21976b5
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-12-29 14:15:18 +0700

    part 4: ngFor sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { DataBindingComponent } from "./components/data-binding/data-binding.component";
 import { AdminComponent } from "./components/admin/admin.component";
 import { NgIfComponent } from "./components/ng-if/ng-if.component";
+import { NgForComponent } from "./components/ng-for/ng-for.component";
 
 @Component({
   selector: 'app-root',
-  imports: [DataBindingComponent, AdminComponent, NgIfComponent],
+  imports: [DataBindingComponent, AdminComponent, NgIfComponent, NgForComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/ng-for/ng-for.component.ts
+++ src/app/components/ng-for/ng-for.component.ts
@@ -1,11 +1,19 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ng-for',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './ng-for.component.html',
   styleUrl: './ng-for.component.css'
 })
 export class NgForComponent {
+  cityList: string[] = ["New York", "London", "Paris", "Tokyo"];
 
+  employeeList: any[] = [
+    { id: 1, name: "John", age: 25, city: "New York" },
+    { id: 2, name: "Jane", age: 30, city: "London" },
+    { id: 3, name: "Bob", age: 35, city: "Paris" },
+    { id: 4, name: "Alice", age: 40, city: "Tokyo" },
+  ];
 }
