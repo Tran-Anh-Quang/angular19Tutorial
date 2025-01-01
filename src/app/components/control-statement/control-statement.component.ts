08fcf4a814643343f1ef87ef4a67cc55408ad663
Tran Anh Quang <dinosaurous9x@gmail.com>
2025-01-01 20:08:38 +0700

    part-6: switchcase type sample

--- src/app/components/control-statement/control-statement.component.ts
+++ src/app/components/control-statement/control-statement.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-control-statement',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './control-statement.component.html',
   styleUrl: './control-statement.component.css',
   standalone: true,
 })
 export class ControlStatementComponent {
   div1Visible: boolean = true;
+  isChecked: boolean = false;
+  dayName: string = '';
 
   hideAndShowDiv1(isVisible: boolean) {
     this.div1Visible = isVisible;
