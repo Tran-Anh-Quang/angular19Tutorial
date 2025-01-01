cd10ce3c57a874c1db579e0e9ca5426ff147aa10
Tran Anh Quang <dinosaurous9x@gmail.com>
2025-01-01 20:19:07 +0700

    part-6: @for type sample

--- src/app/components/control-statement/control-statement.component.ts
+++ src/app/components/control-statement/control-statement.component.ts
@@ -12,6 +12,13 @@ export class ControlStatementComponent {
   div1Visible: boolean = true;
   isChecked: boolean = false;
   dayName: string = '';
+  cityLists: string[] = ['mon', 'tue', 'wed', 'thu', 'fri'];
+  employeeList: any[] = [
+    { id: 1, name: "John", age: 25, city: "New York" },
+    { id: 2, name: "Jane", age: 30, city: "London" },
+    { id: 3, name: "Bob", age: 35, city: "Paris" },
+    { id: 4, name: "Alice", age: 40, city: "Tokyo" },
+  ];
 
   hideAndShowDiv1(isVisible: boolean) {
     this.div1Visible = isVisible;
