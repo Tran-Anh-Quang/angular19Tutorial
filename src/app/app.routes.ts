ececb7f2ed61d6e55e92803e4ecf735ab52fea03
Tran Anh Quang <dinosaurous9x@gmail.com>
2025-01-13 00:59:46 +0700

    part-10: template form and validation sample

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AdminComponent } from './components/admin/admin.component';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
 import { NgClassComponent } from './components/ng-class/ng-class.component';
+import { TemplateFormComponent } from './components/template-form/template-form.component';
 
 export const routes: Routes = [
     {
@@ -20,5 +21,9 @@ export const routes: Routes = [
     {
         path: 'ng-class',
         component: NgClassComponent,
+    },
+    {
+        path: 'template-form',
+        component: TemplateFormComponent,
     }
 ];
